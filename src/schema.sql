7cec5c41cfa7cae9e6c2aeac0eaeee17f19322e1
RussellTickX <russell@tickx.com>
2020-02-20 11:25:51 +0000

    Fixes schema

--- src/schema.sql
+++ src/schema.sql
@@ -11,29 +11,28 @@ CREATE TABLE events
     description VARCHAR(255),
     venueName VARCHAR(255),
     PRIMARY KEY(event_id)
-)
+);
 
 CREATE TABLE tickets
 (
     ticket_id INT NOT NULL AUTO_INCREMENT,
-    price,
-    link,
-    seller,
+    price VARCHAR(255),
+    link VARCHAR(255),
+    seller VARCHAR(255),
     PRIMARY KEY(ticket_id)
-)
+);
 
 CREATE TABLE events_tickets
 (
     event_tickets_id INT NOT NULL AUTO_INCREMENT,
-    event_id,
-    ticket_id,
-    PRIMARY KEY(event_tickets_id)
+    event_id INT,
+    ticket_id INT,
+    PRIMARY KEY(event_tickets_id),
 
     FOREIGN KEY (event_id)
-      REFERENCES event(event_id)
-
+      REFERENCES event(event_id),
 
     FOREIGN KEY (ticket_id)
       REFERENCES ticket(ticket_id)
       ON DELETE CASCADE
-)
+);
